1acbf7277b53c5195f3648d9e0a707a35ee2028e
Sebastian Nagel <snagel@apache.org>
2018-04-13 13:23:55 +0200

    Add unit tests for invalid or undeclared XML entities (catch #148)

---
+++ src/test/resources/rss/atom.php
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="UTF-8"?><feed
+  xmlns="http://www.w3.org/2005/Atom"
+  xmlns:thr="http://purl.org/syndication/thread/1.0"
+  xml:lang="en-US"
+  xml:base="http://example.com/wp-atom.php"
+   >
+	<title type="text">Atom Example</title>
+	<subtitle type="text">Test Sitemap Parser for Atom Feeds</subtitle>
+
+	<updated>2016-11-17T20:23:31Z</updated>
+
+	<link rel="alternate" type="text/html" href="http://example.com/" />
+	<id>http://example.com/feed/atom/</id>
+	<link rel="self" type="application/atom+xml" href="http://example.com/feed/atom/" />
+
+	<generator uri="https://wordpress.org/" version="4.6.1">WordPress</generator>
+	<entry>
+		<author>
+			<name>John Smith, Writer</name>
+		</author>
+		<title type="html"><![CDATA[Coffee time: milk or not?]]></title>
+		<link rel="alternate" type="text/html" href="http://example.com/coffee-time/" />
+		<id>http://example.com/?p=2</id>
+		<updated>2016-11-13T20:43:42Z</updated>
+		<published>2016-11-14T16:16:00Z</published>
+		<category scheme="http://example.com" term="FoodNDrink" />
+    <summary type="html"><![CDATA[Want to have a coffee [&#8230;]]]></summary>
+		<content type="html" xml:base="http://example.com/coffee-time/"><![CDATA[<p>Want to have a coffee?</p>
+<p>&laquo;Lisa's caf&eacute;&raquo; is open Monday through Saturday from 7 a.m. to 5 p.m.</p>]]></content>
+		<link rel="replies" type="text/html" href="http://example.com/coffee-time/#comments" thr:count="0"/>
+		<link rel="replies" type="application/atom+xml" href="http://example.com/coffee-time/feed/atom/" thr:count="0"/>
+		<thr:total>0</thr:total>
+	</entry>
+	<entry>
+		<author>
+			<name>Ed Miller, Writer & Editor</name>
+		</author>
+		<title type="html"><![CDATA[Tea time: black or green?]]></title>
+		<link rel="alternate" type="text/html" href="http://example.com/tea-time/" />
+		<id>http://example.com/?p=1</id>
+		<updated>2016-11-17T20:23:31Z</updated>
+		<published>2016-11-17T14:06:28Z</published>
+		<category scheme="http://example.com" term="FoodNDrink" />
+    <summary type="html"><![CDATA[Want to have a tea [&#8230;]]]></summary>
+		<content type="html" xml:base="http://example.com/tea-time/"><![CDATA[<p>Want to have a tea?</p>
+<p>The tea house is open Monday through Saturday from 10 a.m. to 5 p.m., and until 7 p.m. on Thursdays.</p>]]></content>
+		<link rel="replies" type="text/html" href="http://example.com/tea-time/#comments" thr:count="0"/>
+		<link rel="replies" type="application/atom+xml" href="http://example.com/tea-time/feed/atom/" thr:count="0"/>
+		<thr:total>0</thr:total>
+	</entry>
+</feed>
